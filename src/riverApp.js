3a1f2ddf9bb5a04425b0f94ea28f9cbce130e876
Eliab Bautista <Eliab Bautista>
2017-04-20 16:54:40 -0500

    changes1.

---
+++ src/riverApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class riverApp extends Component {
+  render() {
+    return (
+      <div>
+        <p> HELLO </p>
+      </div>
+    );
+  }
+}
+
+export default riverApp;
